cc05fc9024f08cc9b37566c537375f0aaeaab0ef
likhon <mehedi.hasansjs@gmail.com>
2022-08-24 01:25:19 +0600

    feat: add cypress to test e2e

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
+  firebase: {
+    projectId: 'diu-alumni-cd457',
+    appId: '1:92048917612:web:aefb2d329b2e2e6e7f4449',
+    storageBucket: 'diu-alumni-cd457.appspot.com',
+    locationId: 'us-central',
+    apiKey: 'AIzaSyDo0I8L-rn9IBzFltHY8FYUxbuWykgukBo',
+    authDomain: 'diu-alumni-cd457.firebaseapp.com',
+    messagingSenderId: '92048917612',
+    measurementId: 'G-S2ZF2FV9YZ',
+  },
   production: true
 };
