3d0529822cd2ee09c7e941bc8f8810b8ef4a1615
likhon <mehedi.hasansjs@gmail.com>
2022-08-25 01:28:25 +0600

    feat: add login

--- src/app/modules/users/pages/me/me.component.ts
+++ src/app/modules/users/pages/me/me.component.ts
@@ -7,12 +7,14 @@ import {AuthService} from "../../../auth/services/auth.service";
 export interface MenuGroup {
   menus: Menu[];
   title: string;
+  shown?: string;
 }
 
 export interface Menu {
   title: string;
   url: string;
   icon: string;
+  shown?: string;
 }
 
 @Component({
@@ -25,16 +27,18 @@ export class MeComponent implements OnInit {
   menuGroups: MenuGroup[] = [
     {
       title: 'My Profile',
+      shown: 'emailVerified',
       menus: [
-        { title: 'Edit My Profile', icon: 'user-pen', url: '/users/me/edit' },
-        { title: 'My Blogs', icon: 'books', url: '/blogs/authors/me' },
-        { title: 'My Events', icon: 'smile', url: '/events/me' },
-        { title: 'My Mails', icon: 'envelope', url: '/contact/me' },
-        { title: 'My Payments', icon: 'dollar', url: '/payment/me' },
+        { title: 'Edit My Profile', icon: 'user-pen', url: '/users/me/edit', shown: 'emailVerified' },
+        { title: 'My Blogs', icon: 'books', url: '/blogs/authors/me', shown: 'approved' },
+        { title: 'My Events', icon: 'smile', url: '/events/me', shown: 'approved' },
+        { title: 'My Mails', icon: 'envelope', url: '/contact/me', shown: 'emailVerified' },
+        { title: 'My Payments', icon: 'dollar', url: '/payment/me', shown: 'emailVerified' },
       ]
     },
     {
       title: 'Users',
+      shown: 'admin',
       menus: [
         { title: 'Approved Users', icon: 'user-check', url: '/users/approved' },
         { title: 'Pending Users', icon: 'user-times', url: '/users/pending' },
@@ -42,6 +46,7 @@ export class MeComponent implements OnInit {
     },
     {
       title: 'Payment',
+      shown: 'treasurer',
       menus: [
         { title: 'All Payment', icon: 'dollar', url: '/payment/all' },
         { title: 'Confirmed Payment', icon: 'check-circle', url: '/payment/confirmed' },
@@ -49,6 +54,7 @@ export class MeComponent implements OnInit {
     },
     {
       title: 'Blogs',
+      shown: 'executiveMember',
       menus: [
         { title: 'All Blogs', icon: 'books', url: '/blogs/all' },
         { title: 'All Authors', icon: 'users', url: '/blogs/authors' },
@@ -56,6 +62,7 @@ export class MeComponent implements OnInit {
     },
     {
       title: 'Events',
+      shown: 'executiveMember',
       menus: [
         { title: 'Past Events', icon: 'reply-clock', url: '/events/past' },
         { title: 'Future Events', icon: 'hourglass-clock', url: '/events/future' },
@@ -63,6 +70,7 @@ export class MeComponent implements OnInit {
     },
     {
       title: 'Contact',
+      shown: 'admin',
       menus: [
         { title: 'All Mail', icon: 'envelope', url: '/contact/all' },
       ]
